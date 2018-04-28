312c261492f65c29e79d96d82335bd165a1470f4
LizCalleja <lizi301196@gmail.com>
2018-04-28 16:20:35 -0500

    formularios

--- resources/views/inventory.blade.php
+++ resources/views/inventory.blade.php
@@ -29,43 +29,48 @@
           <label>Cantidad:</label>
           <input type="text" class="form-control">
           <br>
-          <button type="submit" class="btn btn-outline-primary">
-          Agregar</button>
+          <div class="text-right">
+            <button type="submit" class="btn btn-outline-primary">
+              Agregar
+            </button>
+          </div>
+  </form>
       </div>
     </div>
     <div class="contenedor">
           <div class="form-group">
             <h3>Productos:</h3>
-        <table class="table table-bordered table-responsive">
-        <thead class="thead-default">
-          <tr>
-             <th>Código:</th>
-             <th>Categoría:</th>
-               <th>Marca:</th>
-               <th>Nombre:</th>
-               <th>Presentación:</th>
-               <th>Tipo:</th>
-               <th>Precio:</th>
-               <th>Costo:</th>
-               <th>Existencia:</th>
-              
-          </tr>
-       </thead>
-      <tbody>
-          <tr>
-              <td>LA002</td>
-            <td>Lácteos</td>
-            <td>Lala</td>
-              <td>Yomi Lala</td>
-              <td>200 ml.</td>
-              <td>Pieza</td>
-              <td>$50</td>
-              <td>$29</td>
-              <td>200</td>
-  </tr>
-</tbody>
-</table>
+        <div class="table-responsive">
+          <table class="table table-bordered">
+            <thead class="thead-light">
+              <tr>
+                 <th>Código:</th>
+                 <th>Categoría:</th>
+                   <th>Marca:</th>
+                   <th>Nombre:</th>
+                   <th>Presentación:</th>
+                   <th>Tipo:</th>
+                   <th>Precio:</th>
+                   <th>Costo:</th>
+                   <th>Existencia:</th>
+                  
+              </tr>
+           </thead>
+           <tbody>
+              <tr>
+                  <td>LA002</td>
+                  <td>Lácteos</td>
+                  <td>Lala</td>
+                  <td>Yomi Lala</td>
+                  <td>200 ml.</td>
+                  <td>Pieza</td>
+                  <td>$50</td>
+                  <td>$29</td>
+                  <td>200</td>
+              </tr>
+          </tbody>
+        </table>
+      </div>
 </div>
-</form>
 </div>
 @endsection
